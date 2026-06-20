9f38de54923df0e21ec4ece518459b5d4897e09a
Abhishek Shukla <abhishek.shukladavv@gmail.com>
2026-06-20 21:49:30 +0530

    Initialize Spring Boot project with Maven and application config

---
+++ src/main/java/com/example/weather_app/WeatherAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.weather_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WeatherAppApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+}
